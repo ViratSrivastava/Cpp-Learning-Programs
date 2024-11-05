32ae9b97f706a19e251f10b7dd26cc40ec52b867
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-11-05 16:57:20 +0530

    I

--- basic.cpp
+++ basic.cpp
@@ -4,13 +4,32 @@ int main()
 {
     int n, key, l, h, i, mid;
     cin >> n;
+    cin >>key;
     l = 0;
     h = n - 1;
-    mid = (l+h)/2;
+
     int A[n];
     for (i = 0;i<n;i++)
     {
         cin >> A[i];
     }
-
+    while(l<=h)
+    {   
+        mid = (l+h)/2;
+        if(key == A[mid])
+        {
+            cout << mid;
+            return 0;
+        }
+        else if(A[mid]<key)
+        {
+            l = mid+1;
+        }
+        else
+        {
+            h = mid-1;
+        }
+    }
+    cout << "not found";
+    return 0;
 } 
