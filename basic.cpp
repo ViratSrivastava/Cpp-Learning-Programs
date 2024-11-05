a62c3f74a8e9aa11796b1ee7723a5c0a456d00a6
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-11-05 17:02:38 +0530

    .

--- basic.cpp
+++ basic.cpp
@@ -2,34 +2,6 @@
 using namespace std;
 int main()
 {
-    int n, key, l, h, i, mid;
-    cin >> n;
-    cin >>key;
-    l = 0;
-    h = n - 1;
-
-    int A[n];
-    for (i = 0;i<n;i++)
-    {
-        cin >> A[i];
-    }
-    while(l<=h)
-    {   
-        mid = (l+h)/2;
-        if(key == A[mid])
-        {
-            cout << mid;
-            return 0;
-        }
-        else if(A[mid]<key)
-        {
-            l = mid+1;
-        }
-        else
-        {
-            h = mid-1;
-        }
-    }
-    cout << "not found";
+    
     return 0;
 } 
