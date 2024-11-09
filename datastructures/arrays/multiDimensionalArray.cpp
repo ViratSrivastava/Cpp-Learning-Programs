a147df39f32a14f8b0d25f391bc5949f4d0b25b0
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-11-09 23:35:23 +0530

    5

--- datastructures/arrays/multiDimensionalArray.cpp
+++ datastructures/arrays/multiDimensionalArray.cpp
@@ -13,5 +13,14 @@ int main()
         }
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << A[n][m];
+        }
+        cout << " " << endl;
+    }
+
     return 0;
 }
