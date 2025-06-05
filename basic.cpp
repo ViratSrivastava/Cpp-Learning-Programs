dd7729040acf21f846c3410202c389af364ed056
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-06-05 16:35:53 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -2,16 +2,53 @@
 using namespace std;
 int main()
 {
-    int n,m;
-    cin >> n >> m;
-    int A[n][m];
-    for(int i = 0; i < n; i++)
+    int n, m, p, q;
+    cin >> n >> m >> p >> q;
+    if (m != p)
     {
-        for(int j = 0; j < m;j++)
+        cout << "matrix multiplication not possible";
+        exit(0);
+    }
+    int k = n;
+    int l = q;
+    int A[n][m];
+    int B[p][q];
+    int C[k][l];
+    for (auto &i : A)
+    {
+        for (auto &j : i)
         {
-            cin >> A[n][m];
+            cin >> j;
         }
     }
-
+    for (auto &i : B)
+    {
+        for (auto &j : i)
+        {
+            cin >> j;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < p; k++)
+            {
+                for (int l = 0; l < q; l++)
+                {
+                    C[k][l] += A[i][j] * B[p][q];
+                }
+            }
+        }
+    }
+    cout << "resultant matrix is :" << endl;
+    for (auto &i : C)
+    {
+        for (auto &j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
