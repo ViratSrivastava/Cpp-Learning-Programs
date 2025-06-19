afd20c628554f591acc64d9095b4b0a5f2777af0
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-06-19 16:52:09 +0530

    Create Solution.cpp

---
+++ leetcode/binary-subarrays-with-sum/Accepted/7-4-2025, 5_07_18 AM/Solution.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/binary-subarrays-with-sum
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> prefixCount;
+        prefixCount[0] = 1;
+        int sum = 0, count = 0;
+        for(int num : nums){
+            sum +=num;
+            if(prefixCount.find(sum-goal) != prefixCount.end()) {
+                count+=prefixCount[sum - goal];
+            }
+            prefixCount[sum]++;
+        }
+        return count;
+    }
+};
