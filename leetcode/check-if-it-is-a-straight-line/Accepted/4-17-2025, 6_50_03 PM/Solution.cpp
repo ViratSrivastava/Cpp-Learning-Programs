79cb52961d309502e839c2ff92d54d061af0282f
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-06-21 16:52:55 +0530

    Create Solution.cpp

---
+++ leetcode/check-if-it-is-a-straight-line/Accepted/4-17-2025, 6_50_03 PM/Solution.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/check-if-it-is-a-straight-line
+
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int diff = 0;
+        int  n = coordinates.size();
+        if(n == 2){
+            return true;
+        }
+        int dif = abs(coordinates[0][0] - coordinates[0][1]); 
+        for(int i = 2; i < n; i++){
+            if((coordinates[1][0] - coordinates[0][0]) * (coordinates[i][1] - coordinates[0][1]) == (coordinates[1][1] - coordinates[0][1]) * (coordinates[i][0] - coordinates[0][0])){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
