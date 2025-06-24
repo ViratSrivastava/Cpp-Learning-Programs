5791bb943f1c251518be3c073f6455f12c640deb
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-06-24 17:35:31 +0530

    Create Solution.cpp

---
+++ leetcode/contains-duplicate/Runtime Error/4-26-2024, 7_40_22 PM/Solution.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/contains-duplicate
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int count[10001] = {0};
+        for (int i : nums){
+            count[i]++;
+            if (count[i] >= 2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
