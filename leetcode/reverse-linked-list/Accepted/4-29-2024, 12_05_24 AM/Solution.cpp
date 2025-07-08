6cacc39d40ebc612e65bddde961bcbb79dba3bc6
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-08 17:44:44 +0530

    Create Solution.cpp

---
+++ leetcode/reverse-linked-list/Accepted/4-29-2024, 12_05_24 AM/Solution.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/reverse-linked-list
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* prev = nullptr;         
+        ListNode* current = head;
+        ListNode* next = NULL;
+
+        while (current != nullptr){
+            next = current->next; 
+            current->next = prev;
+            prev = current;       
+            current = next;       
+        }
+        return prev; 
+    }
+};
