40f258b7f2764dfdee235145e83566f71f6da3e1
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-15 17:49:53 +0530

    Create Solution.cpp

---
+++ leetcode/regular-expression-matching/Accepted/6-9-2024, 11_49_50 PM/Solution.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/regular-expression-matching
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        // Base case: empty pattern matches empty string
+        dp[0][0] = true;
+
+        // Base case: pattern with '*' can match empty string
+        for (int j = 2; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        // Fill the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                } else if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
