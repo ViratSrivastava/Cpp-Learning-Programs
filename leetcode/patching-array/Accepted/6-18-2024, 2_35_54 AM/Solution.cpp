bb3e8fa43ce7998570a62d4c7ace7a0407266867
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-17 17:52:35 +0530

    Create Solution.cpp

---
+++ leetcode/patching-array/Accepted/6-18-2024, 2_35_54 AM/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/patching-array
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long maxReach = 0;
+        int patches = 0, i = 0;
+        
+        while (maxReach < n) {
+            if (i < nums.size() && nums[i] <= maxReach + 1) {
+                maxReach += nums[i];
+                i++;
+            } else {
+                maxReach += maxReach + 1;
+                patches++;
+            }
+        }
+        
+        return patches;
+    }
+};
