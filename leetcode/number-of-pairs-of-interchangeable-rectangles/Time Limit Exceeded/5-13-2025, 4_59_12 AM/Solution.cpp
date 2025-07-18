562d80387c828686da53b0d0c1c191bde6f8696b
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-18 17:53:48 +0530

    Create Solution.cpp

---
+++ leetcode/number-of-pairs-of-interchangeable-rectangles/Time Limit Exceeded/5-13-2025, 4_59_12 AM/Solution.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles
+
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        vector<double> cal;
+        int n = rectangles.size();
+        for(int i = 0; i < n; i++){
+            double div = double(rectangles[i][0])/double(rectangles[i][1]);
+            cal.push_back(div);
+        }
+        int count = 0;
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(cal[i] == cal[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
