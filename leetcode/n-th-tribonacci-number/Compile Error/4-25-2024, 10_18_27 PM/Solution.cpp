cc887be25e4d978dfb3a1e7884daf2dba17267aa
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-20 17:55:12 +0530

    Create Solution.cpp

---
+++ leetcode/n-th-tribonacci-number/Compile Error/4-25-2024, 10_18_27 PM/Solution.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/n-th-tribonacci-number
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        int i = 2;
+        int j = 0;
+        if (n == 0)
+        {
+            cout << j;
+        }
+        else if (n == 1)
+        {
+            j = 1;
+            cout << j;
+        }
+        else id (n > 1)
+        {
+            j = 1;
+            while (i <= n)
+            {
+                j = j + 
+            }
+            i = 1;
+            
+            for(i<=n)
+            {
+                j = 
+            }
+        }
+            
+    }
+};
