e1a1bbda30c4a7bbdc40bb79ba7edf897ff721b7
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-20 17:54:39 +0530

    Create Solution.cpp

---
+++ leetcode/minimum-time-visiting-all-points/Accepted/7-8-2025, 12_59_27 AM/Solution.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int totalTime = 0;
+        for (int i = 1; i < points.size(); ++i) {
+            int dx = abs(points[i][0] - points[i - 1][0]);
+            int dy = abs(points[i][1] - points[i - 1][1]);
+            totalTime += max(dx, dy);
+        }
+        return totalTime;
+    }
+};
