0722c73795c75b228bbf777d13354e8863a0ef0f
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-21 16:48:05 +0530

    Create Solution.cpp

---
+++ leetcode/add-digits/Accepted/2-2-2025, 10_29_51 PM/Solution.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/add-digits
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while(num > 0){
+            sum += (num % 10);
+            num = num / 10;
+        }
+        if(sum < 10){
+            return sum;
+        }
+        else{
+            return addDigits(sum);
+        }
+    }
+};
