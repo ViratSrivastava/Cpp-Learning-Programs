96b3387cdb925c918def9b18566046c3a5fc4f48
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-23 23:41:43 +0530

    Create kSumSubSeq.cpp

---
+++ datastructures/recurrsion/kSumSubSeq.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+void kSumSubSeq(int i, int k, int sum, vector<int>&arr, vector<int>&sub){
+    sum += sub.back();
+    if(i >= arr.size()){
+
+        if(accumulate(sub.begin(), sub.end(), 0) == k){
+            for(auto e: sub){
+                cout << e << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+
+    sub.push_back(arr[i]);
+    kSumSubSeq(i+1, k, sum, arr, sub);
+    sub.pop_back();
+    kSumSubSeq(i+1, k, sum, arr, sub);
+}
+int main(){
+    int n;
+    cout << "size of array" <<endl;
+    cin >> n;
+    int k;
+    cout << "enter sub seq sum" << endl;
+    cin >> k;
+    int i = 0;
+    vector<int> arr(n);
+    vector<int> sub;
+    cout << "enter " << n << " Elements" << endl;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int sum = 0;
+    int i = 0;
+    kSumSubSeq(i, k, sum, arr, sub);
+    return 0;
+}
