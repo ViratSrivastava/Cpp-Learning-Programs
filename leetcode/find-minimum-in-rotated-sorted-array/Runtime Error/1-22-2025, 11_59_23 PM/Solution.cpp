c1be998d6ecf995b0cf645a4e115c9c8476aec86
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-24 23:56:39 +0530

    Create Solution.cpp

---
+++ leetcode/find-minimum-in-rotated-sorted-array/Runtime Error/1-22-2025, 11_59_23 PM/Solution.cpp
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        /*      BRUTE FORCE
+            if (nums.size() == 1) {
+                return nums[0];
+            }
+            if (nums[0] < nums[nums.size() - 1]) {
+                return nums[0];
+            }
+            for (int i = 1; i < nums.size(); i++) {
+                  if (nums[i] < nums[i - 1]) {
+                    return nums[i];
+                }
+            }
+            return 0;
+        */
+        // Binary Search
+        // sorted array
+        int l = 0;
+        int r = nums.size() - 1;
+        if (nums[l] < nums[r]){
+            return nums[0];
+        }
+        // single element array case
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (nums[mid] < nums[r]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return 0;
+    }
+};
