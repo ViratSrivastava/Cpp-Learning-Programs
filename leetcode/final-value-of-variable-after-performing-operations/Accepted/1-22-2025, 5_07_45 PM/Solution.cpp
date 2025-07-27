109619ea99ae5b5c946e563875a5f7fc90303211
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-27 23:19:59 +0530

    Create Solution.cpp

---
+++ leetcode/final-value-of-variable-after-performing-operations/Accepted/1-22-2025, 5_07_45 PM/Solution.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(int i = 0; i < operations.size(); i++)
+        {
+            if(operations[i] == "X++" || operations[i] == "++X")
+            {
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+        return x;
+    }
+};
