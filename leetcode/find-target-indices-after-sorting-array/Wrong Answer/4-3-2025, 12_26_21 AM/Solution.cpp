a769b353d8270a4c408933564d50a94de07d36a0
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-30 23:56:43 +0530

    Create Solution.cpp

---
+++ leetcode/find-target-indices-after-sorting-array/Wrong Answer/4-3-2025, 12_26_21 AM/Solution.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/find-target-indices-after-sorting-array
+
+#include <iostream>
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int found = -1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == target){
+                found = i;
+                break;
+            }
+        }
+        if (found == -1) return {};
+        
+        int count = 1;
+        for(int i = found+1; i < n; i++){
+            if(nums[i] == target){
+                count++;
+            }
+        }
+        vector<int> index(count);
+        index[0] = found;
+        for(int i = 1; i < count; i++){
+            index[i] = found+1;
+        }
+        return index;
+    }
+};
