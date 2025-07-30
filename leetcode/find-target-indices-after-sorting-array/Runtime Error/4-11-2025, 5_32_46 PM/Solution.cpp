0ba81505541756230230183b5f42a72ea680ba4e
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-07-30 23:56:24 +0530

    Create Solution.cpp

---
+++ leetcode/find-target-indices-after-sorting-array/Runtime Error/4-11-2025, 5_32_46 PM/Solution.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/find-target-indices-after-sorting-array
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> ans;
+        int l = 0;
+        int r = n-1;
+        if(n == 1){
+            if(nums[0] == target){
+                ans.push_back(0);
+                return ans;
+            }
+        }
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                ans.push_back(mid);
+                if(nums[mid-1] != target){
+                    l = mid+1;
+                }
+                else if(nums[mid+1] != target){
+                    r = mid -1; 
+                }
+                
+            }
+            else if(nums[mid] > target){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
