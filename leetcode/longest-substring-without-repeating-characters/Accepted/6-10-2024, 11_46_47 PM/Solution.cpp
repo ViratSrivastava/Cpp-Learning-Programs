302224f698f46840d35bf93a58493c168edff6a4
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-08-03 23:46:39 +0530

    Create Solution.cpp

---
+++ leetcode/longest-substring-without-repeating-characters/Accepted/6-10-2024, 11_46_47 PM/Solution.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> char_set;
+        int left = 0;
+        int max_length = 0;
+
+        for (int right = 0; right < s.length(); ++right) {
+            while (char_set.count(s[right]) > 0) {
+                char_set.erase(s[left]);
+                ++left;
+            }
+
+            char_set.insert(s[right]);
+            max_length = max(max_length, right - left + 1);
+        }
+
+        return max_length;
+    }
+};
