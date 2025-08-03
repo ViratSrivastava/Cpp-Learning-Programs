440c795ecc1d3509df261c37d044c10569e60c70
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-08-03 23:46:32 +0530

    Create Solution.cpp

---
+++ leetcode/intersection-of-two-arrays/Wrong Answer/5-13-2025, 5_32_34 PM/Solution.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/intersection-of-two-arrays
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() == 1){
+            return nums1;
+        }
+        if(nums2.size() == 1){
+            return nums2;
+        }
+
+        vector<int>inter;
+        for(int i = 0; i < nums1.size(); i++){
+            for(int j = 0; j < nums2.size(); j++){
+                if(nums1[i] == nums2[j]){
+                    inter.push_back(nums1[i]);
+                    nums1.erase(nums1.begin() + i);
+                }
+            }
+        }
+        sort(inter.begin(), inter.end());
+        inter.erase(unique(inter.begin(), inter.end()), inter.end());
+        return inter;
+    }
+};
