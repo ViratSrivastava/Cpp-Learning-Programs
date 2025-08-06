afa26d08d75cc6cafea33d71c2f1cbf85a046e4e
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-08-06 18:10:11 +0530

    Create Solution.cpp

---
+++ leetcode/majority-element/Accepted/4-25-2024, 11_00_46 PM/Solution.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/majority-element
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        std::unordered_map<int, int> count;
+        int n = nums.size();
+        for (int i : nums) {
+            count[i]++;
+            if (count[i] > n/2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
