90a33d2396c91079e5805552ad276d3338ac665a
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-08-31 04:06:12 +0530

    Create Solution.cpp

---
+++ leetcode/maximum-average-subarray-i/Time Limit Exceeded/5-15-2025, 3_55_54 AM/Solution.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/maximum-average-subarray-i
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double avg = -DBL_MAX;
+        double temp;
+        if(nums.size() == 1){
+            return static_cast<double>(nums[0]);
+        }
+        vector<double> dub;
+        for(int i = 0; i < nums.size(); i++){
+            dub.push_back(static_cast<double>(nums[i]));
+        }
+        double d = static_cast<double>(k);
+        for(int i = 0; i <= dub.size()-k; i++){
+            double sum = accumulate(dub.begin()+i, dub.begin()+i+k, 0.0);
+            temp = double(sum)/double(d);
+            avg = max(temp,avg);
+        }
+        return avg;
+    }
+};
