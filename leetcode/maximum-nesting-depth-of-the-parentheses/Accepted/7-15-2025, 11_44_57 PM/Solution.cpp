0be1934ea129c84c33e2aee995eb0e3c0b3a7879
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-09-08 04:11:26 +0530

    Create Solution.cpp

---
+++ leetcode/maximum-nesting-depth-of-the-parentheses/Accepted/7-15-2025, 11_44_57 PM/Solution.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        int currDepth = 0;
+        int maxDepth = 0;
+        for (char ch : s) {
+            if (ch == '(') {
+                currDepth++;
+                maxDepth = max(maxDepth, currDepth);
+            } else if (ch == ')') {
+                currDepth--;
+            }
+        }
+        return maxDepth;
+    }
+};
