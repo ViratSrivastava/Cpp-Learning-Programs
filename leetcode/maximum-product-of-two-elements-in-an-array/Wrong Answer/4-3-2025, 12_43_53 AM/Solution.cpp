fbf046e26181f86059412e9beb87c3625e2c8d50
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-09-18 04:33:20 +0530

    Create Solution.cpp

---
+++ leetcode/maximum-product-of-two-elements-in-an-array/Wrong Answer/4-3-2025, 12_43_53 AM/Solution.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int j = n-1;
+        int i = n-2;
+        int product = nums[i] * nums[j];
+        return product;
+    }
+};
