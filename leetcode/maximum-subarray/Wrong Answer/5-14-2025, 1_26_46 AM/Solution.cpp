296bc196fbfdaa24821897d0758309c4ba4537b8
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-09-24 04:35:17 +0530

    Create Solution.cpp

---
+++ leetcode/maximum-subarray/Wrong Answer/5-14-2025, 1_26_46 AM/Solution.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/maximum-subarray
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        int l = 0;
+        int r = nums.size()-1;
+        int sum = INT_MIN;
+        while(r >= l){
+            int temp_sum = accumulate(nums.begin() + l, nums.begin() + r+1, 0);
+            if(temp_sum>sum){
+                sum = temp_sum;
+                temp_sum = 0;
+            }
+            if(nums[l] >= nums[r]){
+                r--;
+            }
+            else{
+                l++;
+            }
+            temp_sum = 0;
+        }
+        return sum;
+    }
+};
