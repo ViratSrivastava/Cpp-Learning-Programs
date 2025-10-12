631839bda7eacf0541f57b75f0f841856c89c4c4
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-10-13 04:40:20 +0530

    Create Solution.cpp

---
+++ leetcode/next-greater-element-i/Accepted/7-13-2025, 11_56_42 PM/Solution.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/next-greater-element-i
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nge; 
+        stack<int> st;
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            int num = nums2[i];
+            while (!st.empty() && st.top() <= num)
+                st.pop();
+            nge[num] = st.empty() ? -1 : st.top();
+            st.push(num);
+        }
+        vector<int> result;
+        for (int num : nums1) {
+            result.push_back(nge[num]);
+        }
+
+        return result;
+    }
+};
